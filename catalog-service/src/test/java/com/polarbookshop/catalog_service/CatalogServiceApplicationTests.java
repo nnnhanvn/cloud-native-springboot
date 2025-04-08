00e29245c6e951e03cf57667ed1b9fa3980e89f9
Nhan <nnnhanvn@gmail.com>
2025-04-08 16:03:16 +0700

    test container

--- catalog-service/src/test/java/com/polarbookshop/catalog_service/CatalogServiceApplicationTests.java
+++ catalog-service/src/test/java/com/polarbookshop/catalog_service/CatalogServiceApplicationTests.java
@@ -2,12 +2,13 @@ package com.polarbookshop.catalog_service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("integration")
 class CatalogServiceApplicationTests {
 
 	@Test
-	void contextLoads() {
-	}
+	void contextLoads() {}
 
 }
